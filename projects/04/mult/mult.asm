2deb6e082ba6b04642669a4935005c5720cfc9e6
gongqingkui <gqk@samsungR467>
2021-09-10 17:17:37 +0800

    Add mult.asm chap4 : no working for mult.asm

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -2,8 +2,24 @@
 // and the book "The Elements of Computing Systems"
 // by Nisan and Schocken, MIT Press.
 // File name: projects/04/Mult.asm
+// Implements: gongqingkui At 126.com
 
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+(LOOP)
+@1
+D=M
+@2
+M=D+M
+@0
+M=A-1
+@0
+D=M
+@LOOP
+D;JGT
+(END)
+@END
+0;JMP

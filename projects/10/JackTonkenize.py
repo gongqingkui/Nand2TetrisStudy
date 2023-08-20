c399e7e525a0b4d1927a499780e594eb349bdbdb
gongqingkui T14 <gongqingkui@126.com>
2023-08-20 22:19:06 +0800

    washing.py

--- projects/10/JackTonkenize.py
+++ projects/10/JackTonkenize.py
@@ -14,20 +14,62 @@ keywords = ['class','constructor','function',
             'if','else','while','return']
 
 symbols = "{}[]().,;+-*/&|<>=~" 
+i,j = 0,1
+lineno,colno = 0,0
 
 def parser(code=None): 
     with open(code, 'r', encoding='gb2312') as f:
         JackCodeBuffer = f.readlines()
-    JackCodeBuffer = "".join(JackCodeBuffer)
-    JackCodeBuffer = re.sub(r'\/[\/*].*','',JackCodeBuffer).replace('\n','').strip()
+    #JackCodeBuffer = "".join(JackCodeBuffer)
+    #JackCodeBuffer = re.sub(r'\/[\/*].*','',JackCodeBuffer).replace('\n','').strip()
+    #JackCodeBuffer = re.sub(r'([\(\)\[\]\{\}\.,;\+\-\*\/\&\|<>=~])',r' \1 ',JackCodeBuffer)
+    #print(JackCodeBuffer)
     return JackCodeBuffer 
 
 
 def hasMoreTokens(JackCodeBuffer):
-    return bool(len(JackCodeBuffer))
+    #return bool(len(JackCodeBuffer))
+    return not(len(JackCodeBuffer) - lineno) or (len(JackCodeBuffer[-1]) - j)
 
+def advance2(JackCodeBuffer): 
+    global i,j,lineno,colno
+    status = ""
+    if JackCodeBuffer[i] == '/':
+        if JackCodeBuffer[j] == '/':
+            j = JackCodeBuffer.index('\n',i)
+            lineno +=1 
+            print(f"COMMENT{lineno},{colno} {i}:{j}:{JackCodeBuffer[i:j]}")
+            
+        if JackCodeBuffer[j] == '*' and JackCodeBuffer[j+1] == '*':
+            j = JackCodeBuffer.index('*/',i)
+            lineno +=1 
+            print(f"COMMENT{lineno},{colno} {i}:{j}:{JackCodeBuffer[i:j]}")
+    if JackCodeBuffer[i] == '\n':
+        lineno += 1
+        print(f"BLANK_LINE")
+    i = j + 1
+    j = i + 1
 
 def advance(JackCodeBuffer): 
+    global i,j,lineno,colno
+    for lineno,line in enumerate(JackCodeBuffer):
+        if re.match(r'^\/\/.*$',line) or re.match(r'^\/\*{2}.*\*\/$',line):
+            print(f'line {lineno} type:comment:{line}') 
+        if re.match(r'^$',line):
+            print(f'line {lineno} type:Blank') 
+        while i < len(line):
+            m = re.search(r'[a-zA-Z0-9]*',line[i:])
+            if m: 
+                tok = m.group(0)
+                if tok in keywords:
+                    print(f'line {lineno} type:keyword:{tok}') 
+                else:
+                    print(f'line {lineno} type:identifier:{tok}') 
+
+
+        
+  
+        
     for kw in keywords:
         if JackCodeBuffer.find(kw) == 0:
             return kw 
@@ -67,7 +109,27 @@ def tokenType(JackCode):
 
 if __name__ == '__main__':
     JackCodeBuffer = parser('ArrayTest/main.jack') 
-    while hasMoreTokens(JackCodeBuffer):
-        c = advance(JackCodeBuffer)
-        JackCodeBuffer = JackCodeBuffer[len(c):].strip()
-        print('%20s%10s' % (c, tokenType(c)))
+    print(JackCodeBuffer)
+
+    c = advance(JackCodeBuffer)
+    print('%20s%10s' % (c, tokenType(c)))
+    c = advance(JackCodeBuffer)
+    print('%20s%10s' % (c, tokenType(c)))
+    c = advance(JackCodeBuffer)
+    print('%20s%10s' % (c, tokenType(c)))
+    c = advance(JackCodeBuffer)
+    print('%20s%10s' % (c, tokenType(c)))
+    c = advance(JackCodeBuffer)
+    print('%20s%10s' % (c, tokenType(c)))
+    c = advance(JackCodeBuffer)
+    print('%20s%10s' % (c, tokenType(c)))
+    c = advance(JackCodeBuffer)
+    print('%20s%10s' % (c, tokenType(c)))
+    c = advance(JackCodeBuffer)
+    print('%20s%10s' % (c, tokenType(c)))
+    c = advance(JackCodeBuffer)
+    print('%20s%10s' % (c, tokenType(c)))
+    #while hasMoreTokens(JackCodeBuffer):
+    #    c = advance(JackCodeBuffer)
+        #JackCodeBuffer = JackCodeBuffer[len(c):].strip()
+    #     print('%20s%10s' % (c, tokenType(c)))
